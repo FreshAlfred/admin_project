4ab59070281494199a34b6630ed583a88532d9d4
alfred <aaaalfred93@outlook.com>
2023-07-24 17:05:46 +0800

    build: build

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import App from '@/App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+// 引用element-plus插件
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn
+})
+
+import 'virtual:svg-icons-register'
+
+import globleComponent from '@/components'
+console.log(globleComponent)
+
+app.mount('#app')
