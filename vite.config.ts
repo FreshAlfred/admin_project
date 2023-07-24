35a94a7709ef840ab85dfd5fd360fb622a619cca
alfred <aaaalfred93@outlook.com>
2023-07-24 17:06:25 +0800

    fix: comitlint

--- vite.config.ts
+++ vite.config.ts
@@ -5,17 +5,18 @@ import path from 'path'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  createSvgIconsPlugin({
-    // Specify the icon folder to be cached
-    iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-    // Specify symbolId format
-    symbolId: 'icon-[dir]-[name]'
-  })
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      // Specify the icon folder to be cached
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // Specify symbolId format
+      symbolId: 'icon-[dir]-[name]',
+    }),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, 'src')
-    }
-  }
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
