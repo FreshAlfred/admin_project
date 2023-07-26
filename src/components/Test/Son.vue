0732e11effa6d3926fd455b14d8a10b9164c625f
alfred <aaaalfred93@outlook.com>
2023-07-26 17:54:29 +0800

    fix: 2023072602

--- src/components/Test/Son.vue
+++ src/components/Test/Son.vue
@@ -1,11 +1,7 @@
 <template>
-    <div>
-        iam son
-    </div>
+  <div>iam son</div>
 </template>
 
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
